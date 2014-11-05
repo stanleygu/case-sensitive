401b7a9eb720536059640dc57f5eb5dccf5baac3
Stanley Gu <stanleygu@users.noreply.github.com>
2014-11-05 00:27:36 -0800

    Create case-sensitive.js
    Based off of angular/angular.js#1925

---
+++ case-sensitive.js
@@ -0,0 +1,17 @@
+'use strict'
+
+angular.module('sg.case.sensitive')
+  .directive('caseSensitive', function() {
+    return {
+      link: function(scope, element, attr) {
+        scope.attr = attr;
+        var caseSensitive = attr.caseSensitive.split(',');
+        angular.forEach(caseSensitive, function(a) {
+          var lowercase = a.toLowerCase();
+          scope.$watch('attr[lowercase]', function() {
+            element[0].setAttribute(a, element[0].getAttribute(lowercase));
+          });
+        });
+      }
+    };
+  });
